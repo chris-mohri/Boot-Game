6969eb8334b3509327dbd864b8e3e51e59ea21ec
cim18 <cim18@pitt.edu>
2023-07-08 16:34:11 -0400

    chris-update

---
+++ Assets/Scripts/Card_Controller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card_Controller : MonoBehaviour
+{
+
+    [SerializeField]
+    private Sprite orange_pic;
+    [SerializeField]
+    private Sprite coconut_pic;
+    [SerializeField]
+    private Sprite banana_pic;
+    [SerializeField]
+    private Sprite kiwi_pic;
+    [SerializeField]
+    private Sprite blueberry_pic;
+    [SerializeField]
+    private Sprite strawberry_pic;
+    [SerializeField]
+    private Sprite cardback_pick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
